35eb71cc25e2bf4845c8ff37dd1fac3ba69d1689
Marek Zelinka <mzelinka17@gmail.com>
2024-07-22 15:05:58 +0200

    solve ex2.9

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
 import { useState } from 'react'
 
 function App() {
-  let [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  let [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  ])
   let [newName, setNewName] = useState('')
   let [newNumber, setNewNumber] = useState('')
+  let [filter, setFilter] = useState('')
+
+  let personsToShow = persons.filter((person) =>
+    person.name.toLowerCase().includes(filter.toLowerCase()),
+  )
 
   return (
     <div>
       <h1>Phonebook</h1>
+      <div>
+        <label htmlFor="q" aria-label="Filter by name">
+          filter shown with
+        </label>{' '}
+        <input
+          type="search"
+          name="q"
+          id="q"
+          value={filter}
+          onChange={(event) => setFilter(event.target.value)}
+        />
+      </div>
+      <h2>Add person</h2>
       <form
         onSubmit={(event) => {
           event.preventDefault()
@@ -22,6 +45,7 @@ function App() {
           let personObject = {
             name: newName,
             number: newNumber,
+            id: persons.length + 1,
           }
           setPersons((persons) => persons.concat(personObject))
 
@@ -59,8 +83,10 @@ function App() {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <div key={person.name}>{person.name}</div>
+      {personsToShow.map((person) => (
+        <div key={person.name}>
+          {person.name} {person.number}
+        </div>
       ))}
     </div>
   )
