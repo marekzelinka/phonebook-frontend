b5af933ceeff418dc5672294a699186f3eb58367
Marek Zelinka <mzelinka17@gmail.com>
2024-07-22 14:55:04 +0200

    solve ex2.6

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,39 @@ function App() {
   return (
     <div>
       <h1>Phonebook</h1>
-      <form>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault()
+
+          let personObject = {
+            name: newName,
+          }
+          setPersons((persons) => persons.concat(personObject))
+
+          setNewName('')
+        }}
+      >
         <div>
-          name: <input />
+          <label htmlFor="name" style={{ display: 'block' }}>
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            required
+            value={newName}
+            onChange={(event) => setNewName(event.target.value)}
+          />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit">Save</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => (
+        <div key={person.name}>{person.name}</div>
+      ))}
     </div>
   )
 }
