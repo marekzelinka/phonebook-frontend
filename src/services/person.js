21bc87737670fe97969e8e2d2ff85d34c3c6212f
Marek Zelinka <mzelinka17@gmail.com>
2024-07-23 07:41:05 +0200

    solve ex3.10

--- src/services/person.js
+++ src/services/person.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:3000/persons'
+const BASE_URL = `${import.meta.env.VITE_API_URL}/api/persons`
 
 export function getPersons() {
   return axios.get(BASE_URL).then((response) => response.data)
